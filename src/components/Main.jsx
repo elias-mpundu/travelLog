2aec8e30a1878cea2a3f1c06bae75c6e51478dc7
Elias Mpundu <97706618+elias-mpundu@users.noreply.github.com>
2024-01-11 12:53:52 -0600

    Google maps links have been updated to be functional

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -12,7 +12,7 @@ console.log(props)
                     <img className="travel-image" src={props.item.img} />
                 </section>
                 <section className='img-text'>
-                    <p><span className='location'>{props.item.location}</span> <a className='maps-location' href=''>View on Google Maps</a></p>
+                    <p><span className='location'>{props.item.location}</span> <a className='maps-location' href={props.item.link}>View on Google Maps</a></p>
                     <h3 className='name'>{props.item.name}</h3>
                     <p className='dates'><span>{props.item.startDate}</span> - <span>{props.item.endDate}</span></p>
                     <p className='description'>{props.item.description}</p>
