4aa171825a711e91bac3126693931d0a42e28b21
Elias Mpundu <97706618+elias-mpundu@users.noreply.github.com>
2024-01-15 20:55:23 -0600

    image links in dataJSX are modified to test which link will show up

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -2,7 +2,7 @@
 const data = [
     {
         key: 1,
-        img: '/images/mountFuji.png',
+        img: './public/images/mountFuji.png',
         location: 'Japan',
         link: 'https://www.google.com.fj/maps/place/Mount+Fuji/@35.3606891,138.7272141,20z/data=!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu',
         name: 'Mount Fuji',
@@ -12,7 +12,7 @@ const data = [
     },
     {
         key: 2,
-        img: '/images/victorialFalls.jpg',
+        img: '../public/images/victorialFalls.jpg',
         location: 'Zambia',
         link: 'https://www.google.com.fj/maps/place/Victoria+Falls/@-17.9255155,25.8559587,17z/data=!3m1!4b1!4m6!3m5!1s0x194feff9da0bf2f9:0x8b054663df18d568!8m2!3d-17.9255206!4d25.8585336!16zL20vMDFiYmd5?entry=ttu',
         name: 'Victoria Falls',
@@ -22,24 +22,14 @@ const data = [
     },
     {
         key: 3,
-        img: '/images/rome.jpg',
+        img: '/public/images/rome.jpg',
         location: 'Italy',
         link: 'https://www.google.com.fj/maps/place/Colosseo,+00184+Roma+RM,+Italy/@41.8905731,12.4897237,17z/data=!3m1!4b1!4m6!3m5!1s0x132f61b6fc6433df:0x165f79d5d2332163!8m2!3d41.8905691!4d12.4922986!16s%2Fg%2F1q63cjjhd?entry=ttu',
         name: 'Colosseum',
         startDate: '12 Jan, 2021',
         endDate : '24 Jan, 2021',
         description: 'The Colosseum was inaugurated in 80 A.D. with over 100 bloody games ranging from gladiatorial combats to animal fights. It was the largest theater in the Roman Empire and was believed to hold about 50,000 people inside.'
-    },
-    // {
-    //     key: 3,
-    //     img: '/images/Basilica.avif',
-    //     location: 'Rome',
-    //     name: 'St. Peters Basilica',
-    //     startDate: '12 Jan, 2021',
-    //     endDate : '24 Jan, 2021',
-    //     description: 'The Colosseum was inaugurated in 80 A.D. with over 100 bloody games ranging from gladiatorial combats to animal fights. It was the largest theater in the Roman Empire and was believed to hold about 50,000 people inside.'
-    // },
-
+    }
 ]
 
 export default data;
